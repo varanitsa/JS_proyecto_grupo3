b307c75687c76a17270f254e9f20feb46ed7a200
AnnaGirona89 <annartworks@gmail.com>
2021-11-08 14:11:26 +0100

    funcionArrayCheckList

---
+++ main.js
@@ -0,0 +1,21 @@
+
+$( document ).ready(function() {
+
+
+$(document).on('click', 'input:checkbox', getCheckedBox);
+
+getCheckedBox();
+
+function getCheckedBox() {
+  
+  let checkedBox = $.map($('input:checkbox:checked'), 
+                         function(val, i) {
+                            return val.value;
+                         });
+  console.clear();
+  console.log(checkedBox);
+}
+
+
+
+});
