84829c9de11f0648535ac7b6c379117daa34a0a4
desimorenoh <desimorenoh@gmail.com>
2021-11-10 16:00:26 +0100

    añado codigo a boton

--- main.js
+++ main.js
@@ -1,24 +1,63 @@
+$(document).ready(function() {
 
-$( document ).ready(function() {
+    $(document).on('click', 'input:checkbox', getCheckedBox, );
+
+    getCheckedBox();
+
+    function getCheckedBox() {
+
+        let checkedBox = $.map($('input:checkbox:checked'),
+            function(val, i) {
+                t
+                return val.value;
+            });
+        console.clear();
+        console.log(checkedBox);
+
+        $(".selectall").click(function() {
+            $(".participante").prop("checked", $(this).prop("checked"));
+        });
+
+        let student = [
+            "Anna Girona",
+            "Sonia",
+            "Desiree",
+            "Alisa",
+            "Alexia",
+            "Ana Casas",
+            "Candy",
+            "Yuliya",
+            "Carmen",
+            "Faby",
+            "Gabrielle",
+            "Gracia",
+            "Helen",
+            "Joana",
+            "Judith",
+            "Laura Campos",
+            "Laura Conteras",
+            "Marisa",
+            "Rosa",
+            "Sandra",
+            "Sara",
+            "Tamara",
+            "Valentina",
+            "Kristina"
+        ];
+
+        $("#start").click(function() {
+            let winner = student[Math.floor(Math.random() * student.length)];
+            console.log(winner)
+        })
+
+        $("#reset").click(function() {
+
+        })
+
+    }
 
 
-$(document).on('click', 'input:checkbox', getCheckedBox);
 
-getCheckedBox();
 
-function getCheckedBox() {
-  
-  let checkedBox = $.map($('input:checkbox:checked'), 
-                         function(val, i) {
-                            return val.value;
-                         });
-  console.clear();
-  console.log(checkedBox);
-}
-
-$(".selectall").click(function(){
-  $(".participante").prop("checked",$(this).prop("checked"));
-  });
-  
 
 });
