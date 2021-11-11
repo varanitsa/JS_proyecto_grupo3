0fcee1e9f8c964e8df55c9f13a80e88e16c95244
desimorenoh <desimorenoh@gmail.com>
2021-11-11 14:28:53 +0100

    checkbox to coment mas ul

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
 
-    $(document).on('click', 'input:checkbox', getCheckedBox, );
+    // $(document).on('click', 'input:checkbox', getCheckedBox, );
 
-    getCheckedBox();
+    // getCheckedBox();
 
-    function getCheckedBox() {
+    // function getCheckedBox() {
 
-        let checkedBox = $.map($('input:checkbox:checked'),
-            function(val, i) {
-                t
-                return val.value;
-            });
-        console.clear();
-        console.log(checkedBox);
+    //     let checkedBox = $.map($('input:checkbox:checked'),
+    //         function(val, i) {
+    //             t
+    //             return val.value;
+    //         });
+    //     console.clear();
+    //     console.log(checkedBox);
 
-        $(".selectall").click(function() {
-            $(".participante").prop("checked", $(this).prop("checked"));
-        });
+    //     $(".selectall").click(function() {
+    //         $(".participante").prop("checked", $(this).prop("checked"));
+    //     });
 
         let student = [
             "Anna Girona",
@@ -48,12 +48,6 @@ $(document).ready(function() {
         $("#start").click(function() {
             let winner = student[Math.floor(Math.random() * student.length)];
             console.log(winner)
-            for (i=0; i&lt;Total; i++) {
-                aleatorio = Math.floor(Math.random()*(listado.length));
-                seleccion = listado[aleatorio];
-                trace(seleccion);
-                listado.splice(aleatorio, 1);
-                }
         })
 
         $("#reset").click(function() {
