0550974591f4dfbdecb4ee849a537cbde60aab19
AnnaGirona89 <annartworks@gmail.com>
2021-11-17 15:31:13 +0100

    ArregloCSS

--- main.js
+++ main.js
@@ -1,74 +1,78 @@
-const arr = ["Anna Girona",
-"Sonia",
-"Desiree",
-"Alisa",
-"Alexia",
-"Ana Casas",
-"Candy",
-"Yuliya",
-"Carmen",
-"Faby",
-"Gabrielle",
-"Gracia",
-"Helen",
-"Joana",
-"Judith",
-"Laura Campos",
-"Laura Conteras",
-"Marisa",
-"Rosa",
-"Sandra",
-"Sara",
-"Tamara",
-"Valentina",
-"Kristina"];
+$(document).ready(function () {
+    
+    let coderArray = ["Alexia", "Alisa", "Ana C.", "Anna G.", "Candy", "Carmen", "Desirée", "Faby", "Gabrielle", "Gràcia", "Helen", "Joana", "Judith", "Kristina", "Laura C.", "Laura M.", "Marisa", "Rosa", "Sandra", "Sara", "Sonia", "Tamara", "Valentina", "Yuliya"];
+    let namesAlreadyPicked = [];
 
-let resto = ["Anna Girona",
-"Sonia",
-"Desiree",
-"Alisa",
-"Alexia",
-"Ana Casas",
-"Candy",
-"Yuliya",
-"Carmen",
-"Faby",
-"Gabrielle",
-"Gracia",
-"Helen",
-"Joana",
-"Judith",
-"Laura Campos",
-"Laura Conteras",
-"Marisa",
-"Rosa",
-"Sandra",
-"Sara",
-"Tamara",
-"Valentina",
-"Kristina"];
+    let buttonToChooseCoders = $('.buttonToChooseCoders');
+    let restartButton = $('.restartButton');
+    let winnerSection = $('.winnerSection');
+    let availableCoders = $('.availableCoders');
 
-let number = 0;
-document.addEventListener("DOMContentLoaded", function() {
-document.getElementById("resto").innerHTML=arr.map((item, i)=>`<li id="${i}">${item}</li>`);
+    function showCoders() {
+        availableCoders.html(coderArray.map(coder => `<div>${coder}</div>`));
+        restartButton.hide();
+    }
+
+    showCoders();
+
+    function chooseCoder() {
+        let position = Math.floor(Math.random() * coderArray.length);
+        let winner = coderArray[position];
+        winnerSection.html(winner);
+        namesAlreadyPicked.push(winner);
+        coderArray.splice(position, 1);
+        showCoders();
+        if (coderArray.length === 0) {
+            restartButton.show();
+            buttonToChooseCoders.hide();
+            winnerSection.empty();
+        }
+    }
+
+    function restart() {
+        coderArray = namesAlreadyPicked.sort();
+        namesAlreadyPicked = [];
+        showCoders();
+        buttonToChooseCoders.show();
+    }
+
+    buttonToChooseCoders.on("click", chooseCoder);
+    restartButton.on("click", restart);
+
+    $('#resetButton').on('click', function () {
+        location.reload();
+    })
 
 });
 
-const randomNumber = () =>{
-if(resto.length>0){
-number = Math.floor(Math.random()*resto.length);
 
-document.getElementById("winner").innerHTML=resto[number];
-resto.splice(number, 1);
-document.getElementById("total").innerHTML=resto.map((item, i)=>`<li>${item}</li>`);
-}else{
-    resto = [...arr];
-}
-}
-
-// const resetEver = () =>{
-// resto = [...arr];
-// document.getElementById("winner").innerHTML="";
-// document.getElementById("total").innerHTML=resto.map((item, i)=>`<li>${item}</li>`);
-
-// }
+function generateTeams () {
+    document.getElementById("teams").innerHTML = "";
+    let players = document.getElementById("players").value
+    players = players.split(/(\r\n|\r|\n)/)
+    players = players.filter(player => /[a-z]/i.test(player))
+    const numTeams = Math.min(Number(document.getElementById("num-teams").value),players.length);
+    for (i = 0; i < numTeams; i++) {
+      const team = document.createElement("p")
+      team.innerHTML = `Grupo ${i +1}: `
+      teams.appendChild(team)
+    }
+    let curTeam = 0;
+    while (players.length>0) {
+      rndIndex = Math.floor(Math.random()*players.length)
+      const player = players.splice(rndIndex, 1);
+      const newTeam = document.getElementById("teams").getElementsByTagName("p")[curTeam]; 
+      let playerText = document.createTextNode(` ${player},`);
+      if (players.length < numTeams) {
+        playerText = document.createTextNode(` ${player}`)
+      }
+      newTeam.appendChild(playerText);
+      if (curTeam < numTeams-1) {
+        curTeam ++
+      } else {
+        curTeam = 0;
+      }
+    }
+  }
+  
+  
