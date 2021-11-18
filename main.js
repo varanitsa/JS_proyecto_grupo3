7f12e4b04b815bea5342edd3eb87650e80771c81
desimorenoh <desimorenoh@gmail.com>
2021-11-18 11:16:40 +0100

    corrigiendo espacio en cabecera

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
 
     let coderArray = ["Alexia", "Alisa", "Ana C.", "Anna G.", "Candy", "Carmen", "Desirée", "Faby", "Gabrielle", "Gràcia", "Helen", "Joana", "Judith", "Kristina", "Laura C.", "Laura M.", "Marisa", "Rosa", "Sandra", "Sara", "Sonia", "Tamara", "Valentina", "Yuliya"];
     let namesAlreadyPicked = [];
-
     let buttonToChooseCoders = $('.buttonToChooseCoders');
     let restartButton = $('.restartButton');
     let winnerSection = $('.winnerSection');
@@ -11,6 +10,7 @@ $(document).ready(function () {
     function showCoders() {
         availableCoders.html(coderArray.map(coder => `<div>${coder}</div>`));
         restartButton.hide();
+        
     }
 
     showCoders();
@@ -21,6 +21,7 @@ $(document).ready(function () {
         winnerSection.html(winner);
         namesAlreadyPicked.push(winner);
         coderArray.splice(position, 1);
+        
         showCoders();
         if (coderArray.length === 0) {
             restartButton.show();
@@ -28,7 +29,7 @@ $(document).ready(function () {
             winnerSection.empty();
         }
     }
-
+  
     function restart() {
         coderArray = namesAlreadyPicked.sort();
         namesAlreadyPicked = [];
@@ -42,9 +43,8 @@ $(document).ready(function () {
     $('#resetButton').on('click', function () {
         location.reload();
     })
-
-});
-
+ 
+})
 
 function generateTeams() {
     document.getElementById("teams").innerHTML = "";
@@ -54,7 +54,7 @@ function generateTeams() {
     const numTeams = Math.min(Number(document.getElementById("num-teams").value), players.length);
     for (i = 0; i < numTeams; i++) {
         const team = document.createElement("p")
-        team.innerHTML = `Team ${i + 1}: `
+        team.innerHTML = `Grupo ${i + 1}: `
         teams.appendChild(team)
     }
     let curTeam = 0;
