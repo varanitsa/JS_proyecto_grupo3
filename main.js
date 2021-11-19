4feef5d5f5507f5a686c14c94435924ee79cb65a
desimorenoh <desimorenoh@gmail.com>
2021-11-19 09:49:32 +0100

    subiendo test

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+
+function seleccionarCoder(listaDeCoders, position) {
+    let elegida = listaDeCoders.splice(position,1)[0];
+    
+    return elegida;
+}
+
+
+
 $(document).ready(function () {
 
     let coderArray = ["Alexia", "Alisa", "Ana C.", "Anna G.", "Candy", "Carmen", "Desirée", "Faby", "Gabrielle", "Gràcia", "Helen", "Joana", "Judith", "Kristina", "Laura C.", "Laura M.", "Marisa", "Rosa", "Sandra", "Sara", "Sonia", "Tamara", "Valentina", "Yuliya"];
@@ -18,10 +27,9 @@ $(document).ready(function () {
 
     function chooseCoder() {
         let position = Math.floor(Math.random() * coderArray.length);
-        let winner = coderArray[position];
-        winnerSection.html(winner);
+        let winner = seleccionarCoder( coderArray, position);
+        winnerSection.html(winner);  
         namesAlreadyPicked.push(winner);
-        coderArray.splice(position, 1);
         
         showCoders();
         if (coderArray.length === 0) {
@@ -46,6 +54,8 @@ $(document).ready(function () {
     })
  
 })
+
+
 
 function generateTeams() {
     document.getElementById("teams").innerHTML = "";
